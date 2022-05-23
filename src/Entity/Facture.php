b03ec3549acf7b43f0bdd6cb244fa93f6f4aabdb
KAlecs12 <alexandre.bruno@ynov.com>
2022-05-23 15:21:46 +0200

    fix

--- src/Entity/Facture.php
+++ src/Entity/Facture.php
@@ -5,21 +5,30 @@ namespace App\Entity;
 use App\Repository\FactureRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass:FactureRepository::class)]
+/**
+*@ORM\Entity(repositoryClass=FactureRepository::class)
+*/
 class Facture
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type:"integer")]
+    /**
+    * @ORM\Id
+    * @ORM\GeneratedValue
+    * @ORM\Column(type="integer")
+     */
     private $id;
-
-    #[ORM\Column(type:"string", length:45)]
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
     private $titre;
 
-    #[ORM\Column(type:"string", length:255)]
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $content;
 
-    #[ORM\ManyToOne(targetEntity:User::class, inversedBy:"factures")]
+    /**
+     *@ORM\ManyToOne(targetEntity=User::class, inversedBy="factures")
+     */
     private $idUser;
 
     public function getId(): ?int
